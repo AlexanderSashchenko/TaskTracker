8912adced2a1b05953944ece4f349b4ae36cae9b
Alexandr Sashchenko <santys87@gmail.com>
2020-06-25 19:01:41 +0300

    Added models User, Task, Status

---
+++ src/main/java/com/inmost/tasktracker/model/Status.java
@@ -0,0 +1,21 @@
+package com.inmost.tasktracker.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "status")
+public class Status {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "status_id")
+    private long statusId;
+    @Column(name = "status_name")
+    private String statusName;
+}
